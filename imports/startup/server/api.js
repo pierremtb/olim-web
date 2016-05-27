9e6d965d1ebedec833ad8aa88f75658b0d9d4722
Pierre Jacquier <pierrejacquier39@gmail.com>
2016-05-27 12:41:44 +0200

    Enable global publish for Android

--- imports/startup/server/api.js
+++ imports/startup/server/api.js
@@ -2,3 +2,4 @@ import '../../api/tasks/methods.js';
 import '../../api/tasks/server/publications.js';
 import '../../api/tags/methods.js';
 import '../../api/tags/server/publications.js';
+import '../../api/global/server/publications.js';
